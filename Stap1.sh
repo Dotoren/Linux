5624bb3c8487cd1d64623444ec89679525441c94
Remy Buitenkamp <r.r.buitenkamp@hanze.nl>
2017-06-29 14:34:41 +0200

    added option for multiple commands

--- Stap1.sh
+++ Stap1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 HST="$(hostname)" 
-sed -i "/127.0.0.1/ a\127.0.0.1 $HST" /etc/hosts
+sed -i "/127.0.0.1/ a\127.0.0.1 $HST" /etc/hosts &&
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo -s &&
 
-git clone https://github.com/Dotoren/Linux.git
+sudo apt-get -y update &&
+echo "update succes"
+sudo apt-get -y upgrade &&
+echo "upgrade succes"
+
+git clone https://github.com/Dotoren/Linux.git &&
 
 echo "stap 1 succes"
